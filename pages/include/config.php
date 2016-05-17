dcb917d3b7f4795ef9ba1dd47da78f865bd7c4c0
korkarn <korkarn.worakarn@gmail.com>
2016-05-17 23:55:11 +0700

    navbar main

--- pages/include/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-	define('DB_HOSTNAME', 'localhost');
-	define('DB_USERNAME', 'egco427');
-	define('DB_PASSWORD', 'egco427');
-	define('DB_DATABASE', 'project_deal');
-?>
--- pages/include/prevprofile.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once('function.php');
-    $user_id = $_GET['id'];
-	$mysqli = connectDatabase();
-	$number = getPrevProfile($mysqli,$user_id);
-	header('Location: ../profile.php?id='.$number);
-?>
