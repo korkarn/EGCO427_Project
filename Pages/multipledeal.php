0653f410a912c7c705ef70ed7076d745b64d168b
korkarn <korkarn.worakarn@gmail.com>
2016-05-18 01:44:57 +0700

    create main css

--- Pages/multipledeal.php
+++ Pages/multipledeal.php
@@ -3,14 +3,55 @@
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>Deal</title>
-	<link rel="stylesheet" href="">
-	<style>
-		
-	</style>
+	<title>AFFA Deal</title>
+	
+	<!-- jquery.js for bootstrap -->
+	<script src="https://code.jquery.com/jquery.js"></script>
+
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+	<!-- Latest compiled and minified JS -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+
+	<!-- main css -->
+	<link href="include/main.css" rel="stylesheet">
+
+	<!-- font style : Architects+Daughter -->
+	<link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Architects+Daughter" />
+
 </head>
 
 <body>
+	<!-- Navigation -->
+    <nav role="navigation" class="navbar navbar-default">
+    	<div class="container">
+		    <!-- Brand and toggle get grouped for better mobile display -->
+		    <div class="navbar-header">
+		        <button type="button" data-target="#navbarCollapse" data-toggle="collapse" class="navbar-toggle">
+		            <span class="sr-only">Toggle navigation</span>
+		            <span class="icon-bar"></span>
+		            <span class="icon-bar"></span>
+		            <span class="icon-bar"></span>
+		        </button>
+		        <a href="#">
+		        	<img class="navbar-brand" alt="logo" src="../images/logo/logo-small.png">
+		      	</a>
+		    </div>
+		    <!-- Collection of nav links and other content for toggling -->
+		    <div id="navbarCollapse" class="collapse navbar-collapse">
+		        <ul class="nav navbar-nav">
+		            <li class="active"><a href="#">Home</a></li>
+		            <li><a href="#">Profile</a></li>
+		            <li><a href="#">Messages</a></li>
+		        </ul>
+		        <ul class="nav navbar-nav navbar-right">
+		            <li><a href="#">Login</a></li>
+		        </ul>
+		    </div>
+	    </div>
+	</nav>
 
+	
 </body>
 </html>
