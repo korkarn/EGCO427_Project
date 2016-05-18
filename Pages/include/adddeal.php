f61de7111baaa6ba9bd65e15d5d68cef7161bde2
korkarn <korkarn.worakarn@gmail.com>
2016-05-19 00:56:26 +0700

    คืนไฟล์

--- Pages/include/adddeal.php
+++ Pages/include/adddeal.php
@@ -1,116 +1,68 @@
 <?php
-	require_once('config.php');
+ 	session_start();
+  	require_once('config.php');
+  
+  	$name = $_POST['name'];
+ 	$promotion = $_POST['promotion'];
+ 	$description = $_POST['description'];
+ 	$newprice = $_POST['newprice'];
+ 	$oldprice = $_POST['oldprice'];
+ 	$addr = $_POST['addr'];
+ 	$tel = $_POST['tel'];
+ 	$web = $_POST['web'];
+ 	$lat = $_POST['lat'];
+ 	$log = $_POST['log'];
+ 	$pic1 = $_POST['pic1'];
+ 	$pic2 = $_POST['pic2'];
+ 	$pic3 = $_POST['pic3'];
+ 	$pic4 = $_POST['pic4'];
+ 	$pic5 = $_POST['pic5'];
+  	$point = $_POST['point'];
+  	$datestart = $_POST['datestart'];
+  	$dateend = $_POST['dateend'];
+  	$selectClass = $_POST['selectClass'];
+  
+  	// Connect to database
+  	$mysqli = connectDB();
+ 
+ 	$name = $mysqli->real_escape_string($name);
+ 	$promotion = $mysqli->real_escape_string($promotion);
+ 	$description = $mysqli->real_escape_string($description);
+ 	$newprice = $mysqli->real_escape_string($newprice);
+ 	$oldprice = $mysqli->real_escape_string($oldprice);
+ 	$addr = $mysqli->real_escape_string($addr);
+ 	$tel = $mysqli->real_escape_string($tel);
+ 	$web = $mysqli->real_escape_string($web);
+ 	$lat = $mysqli->real_escape_string($lat);
+ 	$log = $mysqli->real_escape_string($log);
+ 	$pic1 = $mysqli->real_escape_string($pic1);
+ 	$pic2 = $mysqli->real_escape_string($pic2);
+ 	$pic3 = $mysqli->real_escape_string($pic3);
+ 	$pic4 = $mysqli->real_escape_string($pic4);
+ 	$pic5 = $mysqli->real_escape_string($pic5);
+  	$point = $mysqli->real_escape_string($point);
+  	$datestart = $mysqli->real_escape_string($datestart);
+  	$dateend = $mysqli->real_escape_string($dateend);
+  	$selectClass = $mysqli->real_escape_string($selectClass);
+ 	
+ 	$sql_id = "SELECT id, name_main FROM class WHERE name_sub = '$selectClass'";
+ 	$result= mysqli_query($mysqli, $sql_id) or die("Data not found.");
+ 	$row = $result->fetch_array(MYSQLI_ASSOC);
+ 	$id = $row['id'];
+ 	$_SESSION["name_main"] = $row['name_main'];
+ 	
+ 	$sql_insert = "INSERT INTO deal (name,promotion,description,price_new,price_old,address,tel,website,latitude,longitude,pic1,pic2,pic3,pic4,pic5,points,class_id,date_start,date_end) VALUES ('$name','$promotion','$description','$newprice','$oldprice','$addr','$tel','$web','$lat','$log','$pic1','$pic2','$pic3','$pic4','$pic5','$point','$id','$datestart','$dateend')";
+ 	$result = $mysqli->query($sql_insert);
+ 	$sql = "SELECT id FROM deal WHERE id = (SELECT max(id) FROM deal)";
+ 	$result = $mysqli->query($sql);
+ 	$row = $result->fetch_array(MYSQLI_ASSOC);
+ 	$number = $row['id'];
+ 	$_SESSION["class_id"] = $number;
+ 	$_SESSION["name_sub"] = $selectClass;
 
-	$name = $_POST['name'];
-	$promotion = $_POST['promotion'];
-	$description = $_POST['description'];
-	$newprice = $_POST['newprice'];
-	$oldprice = $_POST['oldprice'];
-	$addr = $_POST['addr'];
-	$tel = $_POST['tel'];
-	$web = $_POST['web'];
-	$lat = $_POST['lat'];
-	$log = $_POST['log'];
-	$pic1 = $_POST['pic1'];
-	$pic2 = $_POST['pic2'];
-	$pic3 = $_POST['pic3'];
-	$pic4 = $_POST['pic4'];
-	$pic5 = $_POST['pic5'];
-	$point = $_POST['point'];
-	$datestart = $_POST['datestart'];
-	$dateend = $_POST['dateend'];
-	$dessert = $_POST['dessert'];
-	$food = $_POST['food'];
-	$drink = $_POST['drink'];
-	$transport = $_POST['transport'];
-	$tour = $_POST['tour'];
-	$hotel = $_POST['hotel'];
-	$makeup = $_POST['makeup'];
-	$fashion = $_POST['fashion'];
-	$beauty = $_POST['beauty'];
-	$entertainment = $_POST['entertainment'];
-	$sport = $_POST['sport'];
-	$electronic = $_POST['electronic'];
+ 	// echo $_SESSION["name_main"];
+ 	// echo $_SESSION["name_sub"];
 
-	// Connect to database
-	$mysqli = connectDB();
-
-	$name = $mysqli->real_escape_string($name);
-	$promotion = $mysqli->real_escape_string($promotion);
-	$description = $mysqli->real_escape_string($description);
-	$newprice = $mysqli->real_escape_string($newprice);
-	$oldprice = $mysqli->real_escape_string($oldprice);
-	$addr = $mysqli->real_escape_string($addr);
-	$tel = $mysqli->real_escape_string($tel);
-	$web = $mysqli->real_escape_string($web);
-	$lat = $mysqli->real_escape_string($lat);
-	$log = $mysqli->real_escape_string($log);
-	$pic1 = $mysqli->real_escape_string($pic1);
-	$pic2 = $mysqli->real_escape_string($pic2);
-	$pic3 = $mysqli->real_escape_string($pic3);
-	$pic4 = $mysqli->real_escape_string($pic4);
-	$pic5 = $mysqli->real_escape_string($pic5);
-	$point = $mysqli->real_escape_string($point);
-	$datestart = $mysqli->real_escape_string($datestart);
-	$dateend = $mysqli->real_escape_string($dateend);
-	$dessert = $mysqli->real_escape_string($dessert);
-	$food = $mysqli->real_escape_string($food);
-	$drink = $mysqli->real_escape_string($drink);
-	$transport = $mysqli->real_escape_string($transport);
-	$tour = $mysqli->real_escape_string($tour);
-	$hotel = $mysqli->real_escape_string($hotel);
-	$makeup = $mysqli->real_escape_string($makeup);
-	$fashion = $mysqli->real_escape_string($fashion);
-	$beauty = $mysqli->real_escape_string($beauty);
-	$entertainment = $mysqli->real_escape_string($entertainment);
-	$sport = $mysqli->real_escape_string($sport);
-	$electronic = $mysqli->real_escape_string($electronic);
-
-	echo $name;
-	echo $promotion;
-	echo $description;
-	echo $newprice;
-	echo $oldprice;
-	echo $addr;
-	echo $tel;
-	echo $web;
-	echo $lat;
-	echo $log;
-	echo $pic1;
-	echo $pic2;
-	echo $pic3;
-	echo $pic4;
-	echo $pic5;
-	echo $point;
-	echo $datestart;
-	echo $dateend;
-	echo $dessert;
-	echo $food;
-	echo $drink;
-	echo $transport;
-	echo $tour;
-	echo $hotel;
-	echo $makeup;
-	echo $fashion;
-	echo $beauty;
-	echo $entertainment;
-	echo $sport;
-	echo $electronic;
-
-	// $sql_uid = "SELECT uid FROM user WHERE creditcard = '$creditCardNo'";
-	// $result= mysqli_query($mysqli, $sql_uid) or die("Data not found.");
-	// $row = $result->fetch_array(MYSQLI_ASSOC);
-	// $uid = $row['uid'];
-
-	// $sql_insert = "INSERT INTO cardstatement (uid,number,date,sellerno,product,price) 
-	// 			VALUES ('$uid','$creditCardNo','$date','$sellerNo','$product','$price')";
-	// $result = $mysqli->query($sql_insert);
-
-	// $sql = "SELECT * FROM cardstatement WHERE transno = (SELECT max(transno) FROM cardstatement)";
-	// $result = $mysqli->query($sql);
-	// $row = $result->fetch_array(MYSQLI_ASSOC);
-	// $number = $row['transno'];
-
-	// header('Location: ../cardstate.php?id='.$number);
-
-?>
+ 	header('Location: ../adddeal-success.php');
+  
+ ?>
--- Pages/multideal.php
+++ Pages/multideal.php
@@ -1,106 +1,106 @@
 <?php
-	session_start();
-	$_SESSION["admin"] = 1;
-?>
-
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>AFFA Deal</title>
-	
-	<!-- jquery.js for bootstrap -->
-	<script src="https://code.jquery.com/jquery.js"></script>
-
-	<!-- Latest compiled and minified CSS -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-
-	<!-- Latest compiled and minified JS -->
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
-
-	<!-- main css -->
-	<link href="include/main.css" rel="stylesheet">
-
-	<!-- multideal css -->
-	<link href="include/multideal.css" rel="stylesheet">
-
-	<!-- font style : Architects+Daughter -->
-	<link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Architects+Daughter" />
-
-</head>
-
-<body ng-app="myApp" ng-controller="dealCtrl">
-	<!-- Navigation -->
-    <nav role="navigation" class="navbar navbar-default">
-    	<div class="container">
-		    <!-- Brand and toggle get grouped for better mobile display -->
-		    <div class="navbar-header">
-		        <button type="button" data-target="#navbarCollapse" data-toggle="collapse" class="navbar-toggle">
-		            <span class="sr-only">Toggle navigation</span>
-		            <span class="icon-bar"></span>
-		            <span class="icon-bar"></span>
-		            <span class="icon-bar"></span>
-		        </button>
-		        <a href="main.php">
-		        	<img class="navbar-brand" alt="logo" src="../images/logo/logo-small.png">&nbsp;&nbsp;
-		      	</a>
-		    </div>
-		    <!-- Collection of nav links and other content for toggling -->
-		    <div id="navbarCollapse" class="collapse navbar-collapse">
-		        <ul class="nav navbar-nav">
-		            <li class="active"><a href="#">Food &nbsp;</a></li>
-		            <li><a href="#">Dessert &nbsp;</a></li>
-		            <li><a href="#">Drink</a></li>
-		        </ul>
-		        <ul class="nav navbar-nav navbar-right">
-		            <li><a href="#">Login</a></li>
-		        </ul>
-		    </div>
-	    </div>
-	</nav>
-	
-	<center>
-		<h1 class="deal-name">FOOD
-			<?php
-            if($_SESSION["admin"] == 1){?>
-			<a href="adddeal.php">
-	    		<img class="insert-deal" alt="add" src="../images/logo/add.png">
-	  		</a>
-	  		<?php
-            }
-            ?> 
-  		</h1>
-	</center>
-	
-
-	<div class="container" ng-repeat="rec in names">
-		<div class="row bg-deal">
-			<div class="col-md-4 col-sm-8">
-				<a href="#" class="thumbnail">
-			      <img src="../images/logo/logo-small.png" alt="deal">
-			    </a>
-			</div>
-			<div class="col-md-8 col-sm-4">
-			    <div class="detail">
-				  <p><b>Promotion : &nbsp; </b>{{rec.Promotion}}&nbsp; </p>
-				  <p><b>Description : &nbsp; </b>&nbsp; </p>
-				</div>
-			</div>
-		</div>
-	</div>
-	
-
-	<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.0.7/angular.min.js"></script>
-
-    <script>
-      var app = angular.module('myApp', []);
-      app.controller('dealCtrl', function($scope, $http) {
-        $http.get("include/multideal.php").then(function (response) {
-          $scope.names = response.data;
-          console.log($scope.names);
-        });
-      });
-    </script>
-</body>
-</html>
+ 	session_start();
+ 	$_SESSION["admin"] = 1;
+ ?>
+ 
+ <!DOCTYPE html>
+ <html>
+ <head>
+ 	<meta charset="utf-8">
+ 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+ 	<title>AFFA Deal</title>
+ 	
+ 	<!-- jquery.js for bootstrap -->
+ 	<script src="https://code.jquery.com/jquery.js"></script>
+ 
+ 	<!-- Latest compiled and minified CSS -->
+ 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+ 
+ 	<!-- Latest compiled and minified JS -->
+ 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+ 
+ 	<!-- main css -->
+ 	<link href="include/main.css" rel="stylesheet">
+ 
+ 	<!-- multideal css -->
+ 	<link href="include/multideal.css" rel="stylesheet">
+ 
+ 	<!-- font style : Architects+Daughter -->
+ 	<link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Architects+Daughter" />
+ 
+ </head>
+ 
+ <body ng-app="myApp" ng-controller="dealCtrl">
+ 	<!-- Navigation -->
+     <nav role="navigation" class="navbar navbar-default">
+     	<div class="container">
+ 		    <!-- Brand and toggle get grouped for better mobile display -->
+ 		    <div class="navbar-header">
+ 		        <button type="button" data-target="#navbarCollapse" data-toggle="collapse" class="navbar-toggle">
+ 		            <span class="sr-only">Toggle navigation</span>
+ 		            <span class="icon-bar"></span>
+  		            <span class="icon-bar"></span>
+  		            <span class="icon-bar"></span>
+  		        </button>
+  		        <a href="main.php">
+  		        	<img class="navbar-brand" alt="logo" src="../images/logo/logo-small.png">&nbsp;&nbsp;
+  		      	</a>
+  		    </div>
+ 		    <!-- Collection of nav links and other content for toggling -->
+ 		    <div id="navbarCollapse" class="collapse navbar-collapse">
+ 		        <ul class="nav navbar-nav">
+ 		            <li class="active"><a href="#">Food &nbsp;</a></li>
+ 		            <li><a href="#">Dessert &nbsp;</a></li>
+ 		            <li><a href="#">Drink</a></li>
+ 		        </ul>
+ 		        <ul class="nav navbar-nav navbar-right">
+ 		            <li><a href="#">Login</a></li>
+ 		        </ul>
+ 		    </div>
+ 	    </div>
+ 	</nav>
+ 	
+ 	<center>
+ 		<h1 class="deal-name">FOOD
+ 			<?php
+             if($_SESSION["admin"] == 1){?>
+ 			<a href="adddeal.php">
+ 	    		<img class="insert-deal" alt="add" src="../images/logo/add.png">
+ 	  		</a>
+ 	  		<?php
+             }
+             ?> 
+   		</h1>
+ 	</center>
+ 	
+ 
+ 	<div class="container" ng-repeat="rec in names">
+ 		<div class="row bg-deal">
+ 			<div class="col-md-4 col-sm-8">
+ 				<a href="#" class="thumbnail">
+ 			      <img src="../images/logo/logo-small.png" alt="deal">
+ 			    </a>
+ 			</div>
+ 			<div class="col-md-8 col-sm-4">
+ 			    <div class="detail">
+ 				  <p><b>Promotion : &nbsp; </b>{{rec.Promotion}}&nbsp; </p>
+ 				  <p><b>Description : &nbsp; </b>&nbsp; </p>
+ 				</div>
+ 			</div>
+ 		</div>
+ 	</div>
+ 	
+ 
+ 	<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.0.7/angular.min.js"></script>
+ 
+     <script>
+       var app = angular.module('myApp', []);
+       app.controller('dealCtrl', function($scope, $http) {
+         $http.get("include/multideal.php").then(function (response) {
+           $scope.names = response.data;
+           console.log($scope.names);
+         });
+       });
+     </script>
+ </body>
+ </html>
